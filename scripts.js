f04a8688dfe0b328b335436c6a2fec2d5137e38f
Sanowarsunny <sanowarsunny1998@gmail.com>
2023-02-27 02:07:33 +0600

    js DOM

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
 function myFunction() {
     document.getElementById("demo").innerHTML = "Paragraph changed.";
   }
+
+  const element = document.getElementById("id01");
+  element.innerHTML = "New Heading";
